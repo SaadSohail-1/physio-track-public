b42fff9d1de61fdb9e54aade9d646292ddad3072
Saad Sohail <saadsohail82733@gmail.com>
2025-12-07 03:13:23 +0500

    redux config 3

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 import { store } from './app/store.js'
-import Provider from 'react-redux'
+import {Provider} from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
