76073281448c9c7cd0401dd89e1101907c492afd
Saad Sohail <saadsohail82733@gmail.com>
2026-01-10 06:16:54 -0600

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="bg-gray-900 text-gray-300 py-6 mt-10">
       <div className="max-w-7xl mx-auto px-6 text-center">
         <p className="text-sm">
-          © {new Date().getFullYear()} Physio30. All rights reserved.
+          © {new Date().getFullYear()} PhysioTrack. All rights reserved.
         </p>
         <span className="text-xs">Not an alternative for medical diagonosis.</span>
       </div>
